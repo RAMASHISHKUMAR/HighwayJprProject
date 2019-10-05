f53a094bc118d77016de54e3d20c83a8ed530faf
RAMASHISH KUMAR <ramashishkumarbag@gmail.com>
2019-10-05 19:36:09 +0530

    Done operation of customer driver & milluser navigation drawar are open

--- app/src/main/java/com/highwayjprproject/DashBoardActivity.java
+++ app/src/main/java/com/highwayjprproject/DashBoardActivity.java
@@ -2,12 +2,18 @@ package com.highwayjprproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,19 +22,11 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
 import com.squareup.picasso.Picasso;
 
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-import android.widget.TextView;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 import utils.Constants;
 import utils.HighwayPrefs;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class DashBoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -40,6 +38,7 @@ public class DashBoardActivity extends AppCompatActivity {
     private NavigationView navigationView;
     String userRole;
     private MenuItem newBooking, myBooking, wallet, notification, rateCard, help, about, share, send,gallery;
+    private MenuItem item;
 
 
     @Override
@@ -47,52 +46,33 @@ public class DashBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
+        dashBoardToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(dashBoardToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, dashBoardToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         nevigationInitView();
         updateNavViewHeader();
         navAccoringRoleId();           // According RoleId Nevigation Icon
 
-      /*  FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
-
-       mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,
-                R.id.nav_gallery,
-                R.id.nav_slideshow,
-                R.id.nav_tools,
-                R.id.nav_share,
-                R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     public void nevigationInitView() {
-        //toolbar
-        dashBoardToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(dashBoardToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // avigation Header
-        navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.inflateHeaderView(R.layout.nav_header_dash_board);
         nevCircularUserImgView = headerView.findViewById(R.id.imageView);
         nevUserName = headerView.findViewById(R.id.userProfileName);
         nevUserMobNo = headerView.findViewById(R.id.userMobileNo);
 
         // navigation menuItem
-        // View headerView = navigationView.getHeaderView(0);
         Menu menues = navigationView.getMenu();
+
         newBooking = menues.findItem(R.id.nav_new_booking);
         myBooking = menues.findItem(R.id.nav_my_booking);
         wallet = menues.findItem(R.id.nav_wallet);
@@ -204,5 +184,13 @@ public class DashBoardActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        return false;
     }
 }
